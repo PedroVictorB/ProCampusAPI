8ccf19c90dd757b94764ab87bbbd77b6033fdb50
Pedro <pedrovictor_caldas@hotmail.com>
2015-11-23 16:08:43 -0300

    fix database uri

--- models/Banco.py
+++ models/Banco.py
@@ -16,7 +16,7 @@ DEBUG = True
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-if os.environ.get('DATBASE_URL') is None:
+if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost/procampus'
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
